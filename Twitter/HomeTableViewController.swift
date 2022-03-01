229db6d7185b768497e2d425bb8db5b512635f04
charlesxu27 <charlesxu27@gmail.com>
2022-02-28 21:58:13 -0800

    Implement Favorite and Retweet Features

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -13,12 +13,18 @@ class HomeTableViewController: UITableViewController {
     var tweetArray = [NSDictionary]() // array of dicts
     var numberOfTweets: Int!
     let myRefreshControl = UIRefreshControl()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 150
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     
     @objc func loadTweets(){ // call API
@@ -34,7 +40,7 @@ class HomeTableViewController: UITableViewController {
 
             for tweet in tweets {
                 self.tweetArray.append(tweet) // add retrieved tweets to array
-                print(tweet)
+//                print(tweet)
             }
             self.tableView.reloadData()
             self.myRefreshControl.endRefreshing()
@@ -43,6 +49,7 @@ class HomeTableViewController: UITableViewController {
             print("Could not retrieve tweets :(")
         })
     }
+    
     
     func loadMoreTweets(){
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
@@ -65,8 +72,6 @@ class HomeTableViewController: UITableViewController {
     }
     
     
-    
-    
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout();
         self.dismiss(animated: true, completion: nil)
@@ -87,6 +92,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell;
     }
